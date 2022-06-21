f35848a59ea08aec9c0c8b1e215f69d90c425c8a
cihancalli <cihan.callii@gmail.com>
2022-06-21 23:37:58 +0300

    Escaping Closure

---
+++ NewsApp/Service/WebService.swift
@@ -0,0 +1,14 @@
+//
+//  WebService.swift
+//  NewsApp
+//
+//  Created by Cihan Çallı on 21.06.2022.
+//
+
+import Foundation
+
+class WebService {
+    func newDownload(url: URL, completion: @escaping ([News]?) -> () ){
+        
+    }
+}
