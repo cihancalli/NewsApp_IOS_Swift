4955e9c6b6a12c84df3827bf49e917bdc415d2d7
cihancalli <cihan.callii@gmail.com>
2022-06-21 23:31:35 +0300

    Model

---
+++ NewsApp/Model/News.swift
@@ -0,0 +1,13 @@
+//
+//  News.swift
+//  NewsApp
+//
+//  Created by Cihan Çallı on 21.06.2022.
+//
+
+import Foundation
+
+struct News : Decodable {
+    let title: String
+    let story: String
+}
