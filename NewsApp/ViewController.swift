68ac7e5a8f1ec4b93aa2d1ebd0c11736cd2d6178
cihancalli <cihan.callii@gmail.com>
2022-06-21 21:57:19 +0300

    Initial Commit

---
+++ NewsApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NewsApp
+//
+//  Created by Cihan Çallı on 21.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
